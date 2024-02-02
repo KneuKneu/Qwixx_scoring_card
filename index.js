ec670313b5bebcf112f8090335d17f91c0c25043
KneuKneu <larsleermakers@gmail.com>
2024-02-02 23:06:56 +0100

    Initial commit
    :pig:

---
+++ index.js
@@ -0,0 +1,17 @@
+// // function generateNumbers(max){
+// //     numbers = [1,2,3,4,5,6]
+// //     for (const i = 2; i < numbers.length ; i++){
+// //         console.log(i)
+// //     }
+// // }
+// const generateButton = document.getElementById("generate2")
+
+// function test(){
+//     console.log(generateButton)
+// }
+
+// generateButton.addEventListener("click",function() {
+//     console.log("test")
+// })
+const btn = document.getElementById("test")
+console.log(btn)
