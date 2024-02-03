a738876035b2e863c507dc339f75ed8357e41cd8
KneuKneu <larsleermakers@gmail.com>
2024-02-03 01:32:27 +0100

    Board generation
    Added the generation of the scoreboard. Currently still WIP
    Board is being generated correctly, now working onthe onclick so that it blocks the field

--- index.js
+++ index.js
@@ -1,17 +1,76 @@
-// // function generateNumbers(max){
-// //     numbers = [1,2,3,4,5,6]
-// //     for (const i = 2; i < numbers.length ; i++){
-// //         console.log(i)
-// //     }
-// // }
-// const generateButton = document.getElementById("generate2")
+const generateCardBtn = document.getElementById("GenerateCardBtn")
+const numberInput = document.getElementById("NumberInput")
+const scoreCardDiv = document.getElementById("ScoreCardDiv")
 
-// function test(){
-//     console.log(generateButton)
-// }
+function generateNumbers(max){
+    numbers = []
+// if the inputed max number is between 12 and 20 generate the numbers for the card
+    if (max >= 12 && max <= 20){
+        let listItems = "<table>"
+        for (let i = 1; i <= 4 ; i++){
+            console.log(i)
+            if (i === 1){
+                listItems += `<tr id="red-row">`
+            }else if(i === 2){
+                listItems += `<tr id="yellow-row">`
+            }else if(i === 3){
+                listItems += `<tr id="green-row">`
+            }else {
+                listItems += `<tr id="blue-row">`
+            }
+            if (i === 3 || i === 4){
+                for (let j = max; j > 1 ; j--){
+                    numbers.push(j)
+                    listItems += `
+                    <td>
+                        ${j}
+                    </td>
+                    `
+                }  
+            }else{
+                for (let j = 1; j < max ; j++){
+                    numbers.push(j+1)
+                    listItems += `
+                    <td id="test ${j+1}" onclick="clicked()">${j+1}
+                    </td>
+                    `
+                }
+            }
 
-// generateButton.addEventListener("click",function() {
-//     console.log("test")
-// })
-const btn = document.getElementById("test")
-console.log(btn)
+            listItems += `</tr>`
+            console.log(i)
+        }
+        listItems += "</table>"
+        scoreCardDiv.innerHTML = listItems
+        console.log(listItems)
+        console.log(numbers)
+        console.log(numbers.reverse())
+        console.log(numbers)
+        console.log(numbers.reverse())
+    }else {
+
+    }
+
+}
+
+function clicked() {
+    console.log(this)
+  // The event object has a property called "target" that refers to the element that triggered the event
+  var clickedElement = event.target;
+
+  // Now you can do something with the clicked element
+  console.log('Element clicked:', clickedElement)
+  // value of the element you clicked
+  for (let i = Number(clickedElement.innerText); i > 1 ; i-- ){
+    console.log(i)
+    const currentElement = document.getElementById(`test ${i}`)
+    
+  }
+  // replace the value with an X
+  clickedElement.innerHTML = "X"
+
+}
+
+generateCardBtn.addEventListener("click",function() {
+    generateNumbers(numberInput.value)
+})
