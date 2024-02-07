5c6aa3dc28f383f73a9d95f07bcc7e59f7899582
KneuKneu <larsleermakers@gmail.com>
2024-02-07 11:06:19 +0100

    Gray background and new functions
    Added a gray background to the scoring card
    Added an onclick eventListener to the generateNumbers function that listens to all buttons.
    Added the createRowButtons function that creates all the buttons based on the max rows, the color and the type.

--- index.js
+++ index.js
@@ -1,76 +1,109 @@
 const generateCardBtn = document.getElementById("GenerateCardBtn")
 const numberInput = document.getElementById("NumberInput")
 const scoreCardDiv = document.getElementById("ScoreCardDiv")
+let allNumButtons = ""
 
-function generateNumbers(max){
-    numbers = []
-// if the inputed max number is between 12 and 20 generate the numbers for the card
-    if (max >= 12 && max <= 20){
-        let listItems = "<table>"
-        for (let i = 1; i <= 4 ; i++){
-            console.log(i)
-            if (i === 1){
-                listItems += `<tr id="red-row">`
-            }else if(i === 2){
-                listItems += `<tr id="yellow-row">`
-            }else if(i === 3){
-                listItems += `<tr id="green-row">`
-            }else {
-                listItems += `<tr id="blue-row">`
-            }
-            if (i === 3 || i === 4){
-                for (let j = max; j > 1 ; j--){
-                    numbers.push(j)
-                    listItems += `
-                    <td>
-                        ${j}
-                    </td>
-                    `
-                }  
-            }else{
-                for (let j = 1; j < max ; j++){
-                    numbers.push(j+1)
-                    listItems += `
-                    <td id="test ${j+1}" onclick="clicked()">${j+1}
-                    </td>
-                    `
-                }
-            }
+generateCardBtn.addEventListener("click", function () {
+    generateNumbers(numberInput.value)
+})
 
-            listItems += `</tr>`
-            console.log(i)
+function createRowButtons(max, color, type) {
+//Type = "regular" or "inverse"
+    let listItem = ""
+    if (type === "inverse"){
+        for (let i = max; i > 1; i--) { 
+            localStorage.setItem(color + "-" + i, i);
+            listItem += `
+        <button id="${color}-row-background" class="btn row-background" value="${color}-${i}">
+            ${i}
+        </button>
+        `
         }
-        listItems += "</table>"
-        scoreCardDiv.innerHTML = listItems
-        console.log(listItems)
-        console.log(numbers)
-        console.log(numbers.reverse())
-        console.log(numbers)
-        console.log(numbers.reverse())
-    }else {
-
     }
+    else if (type === "regular"){
+        for (let j = 2; j <= max ; j++) {
+            localStorage.setItem(color + "-" + j, j);
+            listItem += `
+        <button id="${color}-row-background" class="btn row-background" value="${color}-${j}">
+            ${j}
+        </button>
+        `
+    }
+}
+    return listItem
+}
 
+function generateNumbers(max) {
+    // if the inputed max number is between 12 and 20 generate the numbers for the card
+    localStorage.clear();
+    if (max >= 12 && max <= 20) {
+        let listItems = ""
+        for (let i = 1; i <= 4; i++) {
+            if (i === 1) {
+                listItems += `<div id="red-row" class="row">`
+                listItems += createRowButtons(max, "red", "regular")
+            } else if (i === 2) {
+                listItems += `<div id="yellow-row" class="row">`
+                listItems += createRowButtons(max, "yellow", "regular")
+            } else if (i === 3) {
+                listItems += `<div id="green-row" class="row">`
+                listItems += createRowButtons(max, "green", "inverse")
+            } else {
+                listItems += `<div id="blue-row" class="row">`
+                listItems += createRowButtons(max, "blue", "inverse")
+            }
+
+            listItems += `</div>`
+        }
+        //Add the scoring section to the card
+        listItems += `
+        <div id="score-fields">
+        <span id="score-text">Score</span>
+        <button id="red-score-field" class="score-field"></button>
+        <span>+</span>
+        <button id="yellow-score-field" class="score-field"></button>
+        <span>+</span>
+        <button id="green-score-field" class="score-field"></button>
+        <span>+</span>
+        <button id="blue-score-field" class="score-field"></button>
+    </div>`
+        scoreCardDiv.innerHTML = listItems
+        //Update allNumButtons to set the on click event listener
+        allNumButtons = document.querySelectorAll(".btn");
+        allNumButtons.forEach(button => {
+             button.addEventListener("click", event => { 
+                buttonValue = localStorage.getItem(button.value)
+                if (buttonValue === "X"){  
+                    var number = button.value.replace(/^\D+/g, '');  
+                    console.log("nubmer " + number)
+                    console.log(button.value)
+                    button.innerHTML = number
+                    console.log("dsd"+localStorage.getItem(button.value))
+                    console.log(localStorage.setItem(button,3))
+                }else
+                    button.innerText = "X"
+                    localStorage.setItem(button.value,"X")
+            })
+        })
+        
+
+}
 }
 
 function clicked() {
     console.log(this)
-  // The event object has a property called "target" that refers to the element that triggered the event
-  // 'event' is depricated :( -> ask ChatGPT :)
-  var clickedElement = event.target;
+    // The event object has a property called "target" that refers to the element that triggered the event
+    // 'event' is depricated :( -> ask ChatGPT :)
+    var clickedElement = event.target;
 
-  // value of the element you clicked (old)
-//   for (let i = Number(clickedElement.innerText); i > 1 ; i-- ){
-//     console.log(i)
-//     const currentElement = document.getElementById(`test ${i}`)
-//       // replace the value with an X
-//       currentElement.innerHTML = "X"
-    
-//   }
+    // value of the element you clicked (old)
+    //   for (let i = Number(clickedElement.innerText); i > 1 ; i-- ){
+    //     console.log(i)
+    //     const currentElement = document.getElementById(`test ${i}`)
+    //       // replace the value with an X
+    //       currentElement.innerHTML = "X"
+
+    //   }
     clickedElement.innerHTML = "X"
 
 }
-
-generateCardBtn.addEventListener("click",function() {
-    generateNumbers(numberInput.value)
-})
